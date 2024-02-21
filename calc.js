74ea082ce4c463e2e476a78c658fc16ee8531b77
Maksim Sobko <maxim90sobko@gmail.com>
2024-02-21 21:47:58 +0300

    feat: add additional application behavior logic(see comments)

--- calc.js
+++ calc.js
@@ -7,18 +7,28 @@ for (let i = 0; i < btn.length; i++) {
 
 function calculate(event) {
   let btnValue = event.target.value;
+  let dispVal = display.value;
+  let lastChar = dispVal[dispVal.length - 1];
 
   if (btnValue === "=") {
     display.value = eval(display.value);
-  } else if (
-    display.value[0] === "+" ||
-    display.value[0] === "*" ||
-    display.value[0] === "/" ||
-    display.value[0] === "."
-  ) {
-    display.value = "";
   } else if (btnValue === "C") {
     display.value = "";
+  } else if (btnValue === "X" && display.value != "") {
+    display.value = dispVal.slice(0, dispVal.lastIndexOf(lastChar));
+  } else if (display.value === "" && btnValue.search(/\/|\+|\*|\./g) > -1) {
+    display.value = "";
+  } else if (dispVal != "" && btnValue.search(/\/|\+|\*|\.|\-/g) > -1) {
+    if (
+      dispVal.lastIndexOf(lastChar) != dispVal.search(/\/$|\+$|\*$|\.$|\-$/g)
+    ) {
+      display.value += btnValue;
+    }
+    if (
+      dispVal.lastIndexOf(lastChar) === dispVal.search(/\/$|\+$|\*$|\.$|\-$/g)
+    ) {
+      display.value = dispVal.replace(/.$/g, btnValue);
+    }
   } else {
     display.value += btnValue;
   }
